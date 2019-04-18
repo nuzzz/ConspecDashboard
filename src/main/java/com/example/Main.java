d6e88fcaf78ff3384647e4a278ce61c7e8a4e7e2
Linus Teo <fuzznuzz@gmail.com>
2019-04-18 13:41:11 +0800

    fixed errors

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -58,6 +58,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
